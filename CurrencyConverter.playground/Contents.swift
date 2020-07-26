4f5f2d11b6b57ff7fcad069656febbe1967b2702
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-07-26 19:18:18 -0400

    created a method that caluclate currency conversion based on the argument passed in

--- CurrencyConverter.playground/Contents.swift
+++ CurrencyConverter.playground/Contents.swift
@@ -7,9 +7,28 @@ enum Currency {
 let usToCad = 1.34
 let usToMxn = 22.30
 
-var currency: Currency
+var currency: Currency = .cad
 
 func convert(_ dollars: Double) -> Double {
-    
+    switch currency{
+    case .cad:
+        return dollars * usToCad
+    case .mxn:
+        return dollars * usToMxn
+    }
 }
+
+let currencyformat = NumberFormatter()
+currencyformat.numberStyle = .currency
+
+func convert2(amountString: String) -> String? {
+    let amount = Double(amountString)
+    guard let amountUnwrapped = amount else{
+        return nil
+    }
+    return currencyformat.string(from: NSNumber(value: convert(amountUnwrapped)))
+}
+convert2(amountString: "10")
+currency = .mxn
+convert2(amountString: "10")
 
