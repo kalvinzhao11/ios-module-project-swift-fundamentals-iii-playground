22505cb2ef704c4ea7e73713b76eefc06fd6e07d
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-07-26 18:08:24 -0400

    created currency converter playground

---
+++ CurrencyConverter.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+enum Currency {
+    case cad
+    case mxn
+}
+let usToCad = 1.34
+let usToMxn = 22.30
+
+var currency: Currency
+
+func convert(_ dollars: Double) -> Double {
+    
+}
+
